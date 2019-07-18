1c1daadc0247f70cf4ba69090a73eb59a406fe70
Ryan Weaver <ryan@thatsquality.com>
2019-07-18 11:31:23 -0400

    adding other missed param and warming up cache on deploy so it's available

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -3,3 +3,7 @@
 if (getenv('github_token')) {
     $container->setParameter('github_token', getenv('github_token'));
 }
+
+if (getenv('symfony_docs_secret')) {
+    $container->setParameter('symfony_docs_secret', getenv('symfony_docs_secret'));
+}
