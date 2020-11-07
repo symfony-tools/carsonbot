06d89d37a562bcde82b497e7316f1ece234b9206
Nyholm <tobias.nyholm@gmail.com>
2020-11-07 12:19:51 +0100

    Update IssueApi to support create and close issues

--- src/Api/Issue/GithubIssueApi.php
+++ src/Api/Issue/GithubIssueApi.php
@@ -3,15 +3,50 @@
 namespace App\Api\Issue;
 
 use App\Model\Repository;
+use Github\Api\Issue;
 use Github\Api\Issue\Comments;
+use Github\Api\Search;
 
 class GithubIssueApi implements IssueApi
 {
     private $issueCommentApi;
+    private $botUsername;
+    private $issueApi;
+    private $searchApi;
 
-    public function __construct(Comments $issueCommentApi)
+    public function __construct(Comments $issueCommentApi, Issue $issueApi, Search $searchApi, string $botUsername)
     {
         $this->issueCommentApi = $issueCommentApi;
+        $this->issueApi = $issueApi;
+        $this->searchApi = $searchApi;
+        $this->botUsername = $botUsername;
+    }
+
+    public function open(Repository $repository, string $title, string $body, array $labels)
+    {
+        $params = [
+            'title' => $title,
+            'labels' => $labels,
+            'body' => $body,
+        ];
+
+        $issueNumber = null;
+        $exitingIssues = $this->searchApi->issues(sprintf('repo:%s "%s" is:open author:%s', $repository->getFullName(), $title, $this->botUsername));
+        foreach ($exitingIssues['items'] ?? [] as $issue) {
+            $issueNumber = $issue['number'];
+        }
+
+        if (null === $issueNumber) {
+            $this->issueApi->create($repository->getVendor(), $repository->getName(), $params);
+        } else {
+            unset($params['labels']);
+            $this->issueApi->update($repository->getVendor(), $repository->getName(), $issueNumber, $params);
+        }
+    }
+
+    public function close(Repository $repository, $issueNumber)
+    {
+        $this->issueApi->update($repository->getVendor(), $repository->getName(), $issueNumber, ['state' => 'closed']);
     }
 
     /**
--- src/Api/Issue/NullIssueApi.php
+++ src/Api/Issue/NullIssueApi.php
@@ -6,7 +6,15 @@ use App\Model\Repository;
 
 class NullIssueApi implements IssueApi
 {
+    public function open(Repository $repository, string $title, string $body, array $labels)
+    {
+    }
+
     public function commentOnIssue(Repository $repository, $issueNumber, string $commentBody)
+    {
+    }
+
+    public function close(Repository $repository, $issueNumber)
     {
     }
 }
