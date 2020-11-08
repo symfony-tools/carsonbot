f644cfa9b4ddc4f3c760e8323b3cb1fee20f51ea
Nyholm <tobias.nyholm@gmail.com>
2020-11-08 17:20:37 +0100

    Adding PullRequestApi

---
+++ src/Api/PullRequest/PullRequestApi.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Api\PullRequest;
+
+use App\Model\Repository;
+
+/**
+ * Actions that are specific to pull requests.
+ * Actions like comment/close can be performed using the IssueApi.
+ *
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ */
+interface PullRequestApi
+{
+    public function show(Repository $repository, $number): array;
+
+    public function findReviewer(Repository $repository, $number, string $type);
+}
