df0c7d5d3bb74885cd992a6d44d93e6a7535af14
Oskar Stark <oskarstark@googlemail.com>
2024-09-16 14:55:25 +0200

    Cleanup migrations

--- migrations/Version20201112220914.php
+++ migrations/Version20201112220914.php
@@ -7,20 +7,10 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20201112220914 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
         $this->addSql(<<<SQL
 create table lock_keys
 (
@@ -36,6 +26,6 @@ SQL
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $this->throwIrreversibleMigrationException();
     }
 }
