49dd1820bc1308ed7df794b3ad8ab23d03fb453e
Oskar Stark <oskarstark@googlemail.com>
2024-09-16 15:00:36 +0200

    Use Attribute namespace

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,7 +4,7 @@ namespace App\Controller;
 
 use App\Service\RepositoryProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class DefaultController extends AbstractController
 {
