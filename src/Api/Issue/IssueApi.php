419fb276789a0ae35771a63e286c77a6ccefab4d
Oskar Stark <oskarstark@googlemail.com>
2025-04-28 20:24:34 +0200

    Fix typos

--- src/Api/Issue/IssueApi.php
+++ src/Api/Issue/IssueApi.php
@@ -5,7 +5,7 @@ namespace App\Api\Issue;
 use App\Model\Repository;
 
 /**
- * Create, update, close or comment on issues. Not that "issue" also refers to a
+ * Create, update, close or comment on issues. Note that "issue" also refers to a
  * pull request.
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
