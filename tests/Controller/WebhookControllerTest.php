2709eb822c2069d90a7b7c5ae739823e93aa255d
Fabien Potencier <fabien@potencier.org>
2026-09-20 20:59:18 +0100

    Verify GitHub webhook signatures with SHA-256 instead of SHA-1

--- tests/Controller/WebhookControllerTest.php
+++ tests/Controller/WebhookControllerTest.php
@@ -65,7 +65,7 @@ class WebhookControllerTest extends WebTestCase
     public function testRequestWithInvalidSignatureIsRejected()
     {
         $body = file_get_contents(__DIR__.'/../webhook_examples/issue_comment.created.json');
-        $this->postWebhook('issue_comment', $body, 'sha1='.hash_hmac('sha1', $body, 'not-the-configured-secret'));
+        $this->postWebhook('issue_comment', $body, 'sha256='.hash_hmac('sha256', $body, 'not-the-configured-secret'));
 
         $this->assertResponseStatusCodeSame(403);
     }
@@ -74,7 +74,7 @@ class WebhookControllerTest extends WebTestCase
     {
         $server = ['HTTP_X-Github-Event' => $eventHeader];
         if (null !== $signature) {
-            $server['HTTP_X-Hub-Signature'] = $signature;
+            $server['HTTP_X-Hub-Signature-256'] = $signature;
         }
 
         $this->client->request('POST', '/webhooks/github', [], [], $server, $body);
@@ -86,7 +86,7 @@ class WebhookControllerTest extends WebTestCase
             ->getRepository('carsonbot-playground/symfony')
             ->getSecret();
 
-        return 'sha1='.hash_hmac('sha1', $body, $secret);
+        return 'sha256='.hash_hmac('sha256', $body, $secret);
     }
 
     /**
